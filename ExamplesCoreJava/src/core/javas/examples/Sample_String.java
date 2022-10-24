6e72a97b08b890cab07c1aca414634897c54bc8f
ashraf <ashraf.3malagi@gmail.com>
2022-10-24 21:21:09 +0530

    core java exampels

--- ExamplesCoreJava/src/core/javas/examples/Sample_String.java
+++ ExamplesCoreJava/src/core/javas/examples/Sample_String.java
@@ -3,10 +3,10 @@ package core.javas.examples;
 public class Sample_String {
 	public static void main(String[] args) {
 		String str_Sample = "a";
-		System.out.println("Compare To 'a' b is : " +  "a".compareTo("b"));
+		System.out.println("Compare To 'a' b is : " + "a".compareTo("b"));
 		str_Sample = "b";
-		System.out.println("Compare To 'b' a is : " +  "a".compareTo("a"));
+		System.out.println("Compare To 'b' a is : " + "a".compareTo("a"));
 		str_Sample = "b";
-		System.out.println("Compare To 'b' b is : " +  "b".compareTo("a"));
+		System.out.println("Compare To 'b' b is : " + "b".compareTo("a"));
 	}
 }
